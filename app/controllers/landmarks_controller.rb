ee811a509155ed83d56a13eba9b2eb2bc0be1e70
Rafael Batista <rafaelb24@yahoo.com>
2019-03-04 22:10:36 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -14,4 +14,8 @@ class LandmarksController < ApplicationController
     
     erb :'/landmarks/index'
   end 
+  
+  get '/landmarks/:id' do 
+    @landmark = Landmark.find(params[:id])
+  end 
 end
